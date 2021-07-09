aea1ed6b2ca9734c4c4bb1ea758a10ffc1ccee6e
Lena <Lena@49-10>
2021-07-09 13:49:44 +0300

    Add binary tree

---
+++ Бинарное дерево/BinaryTree/BinaryTree/Main.cpp
@@ -0,0 +1,41 @@
+﻿#include "Tree.h"
+#include <string>
+using std::string;
+
+int main()
+{
+	Tree<int, string> tree;
+
+	tree.Add(8, "aaaa");
+	tree.Add(3, "bbbb");
+	tree.Add(10, "cccc");
+	tree.Add(1, "dddd");
+	tree.Add(6, "eeee");
+	tree.Add(9, "ffff");
+	tree.Add(14, "gggg");
+
+	tree.Print();
+/*
+	tree[1] = "zzzz";
+
+	int minKey = tree.MinKey();
+	cout << "Min key: " << minKey << endl;
+	cout << "Value for min key: " << tree[minKey] << endl;
+	cout << "Value for key = 6: " << tree[6] << endl;	
+
+	tree.Remove(1);
+	tree.Print();
+	
+	tree.Remove(14);
+	tree.Print();
+
+	tree.Add(1, "qqqq");
+	tree.Print();
+
+	tree.Remove(10);
+	tree.Print();
+
+	tree.Remove(8);
+	tree.Print();*/
+
+}
---
+++ Бинарное дерево/BinaryTree/BinaryTree/Tree.h
@@ -0,0 +1,233 @@
+﻿#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::exception;
+
+template<typename TKey, typename TValue>
+class Tree
+{
+public:
+
+	void Add(TKey key, TValue value);
+	void Remove(TKey key);
+
+	void Print() const;
+
+	TKey MinKey() const;
+
+	TValue& operator[] (TKey key);
+	const TValue& operator[] (TKey key) const;
+
+private:
+
+	struct Node //узел дерева
+	{
+		Node* parent = nullptr;
+		Node* left   = nullptr;
+		Node* right  = nullptr;
+
+		TKey key;
+		TValue value;
+
+		bool IsLeaf()
+		{
+			return left == nullptr && right == nullptr;
+		}
+
+		bool HasOnlyOneChild()
+		{
+			return left != nullptr && right == nullptr ||
+				left == nullptr && right != nullptr;
+		}
+
+		bool HasTwoChildren()
+		{
+			return left != nullptr && right != nullptr;
+		}
+	};
+
+	//Корень дерева, указатель на вершину 
+	Node*  _root = nullptr;
+	size_t _size = 0;
+
+	//внутренние методы
+	void Print(Node* node) const;
+	Node* MinNode(Node* node) const
+	{
+		if (node == nullptr)
+			return nullptr;
+
+		Node* minNode = node;
+
+		while (minNode->left != nullptr)
+		{
+			minNode = minNode->left;
+		}
+
+		return minNode;
+	}
+	Node* SearchNode(TKey key) const
+	{
+		Node* cursor = _root;
+		while (cursor != nullptr)
+		{
+			if (key == cursor->key)
+				return cursor;
+			else if (key < cursor->key)
+				cursor = cursor->left;
+			else
+				cursor = cursor->right;
+		}
+
+		return nullptr;
+	}
+
+};
+
+template<typename TKey, typename TValue>
+inline void Tree<TKey, TValue>::Add(TKey key, TValue value)
+{
+	Node* newNode = new Node;
+	newNode->key = key;
+	newNode->value = value;
+
+	if (_root == nullptr)
+	{
+		_root = newNode;
+	}
+	else
+	{
+		Node* cursor = _root;
+		Node* current = _root;
+
+		while (cursor != nullptr)
+		{
+			current = cursor;
+			if (key < cursor->key)
+				cursor = cursor->left;
+			else
+				cursor = cursor->right;
+		}
+
+		newNode->parent = current;
+		if (key < current->key)
+			current->left = newNode;
+		else
+			current->right = newNode;
+	}
+
+	_size++;
+}
+
+template<typename TKey, typename TValue>
+inline void Tree<TKey, TValue>::Remove(TKey key)
+{
+	Node* delNode = SearchNode(key);
+
+	if (delNode == nullptr) return;
+
+	if (delNode->HasTwoChildren())
+	{
+		Node* currentNode = delNode;
+		delNode = MinNode(delNode->right);
+		
+		currentNode->key = delNode->key;
+		currentNode->value = delNode->value;
+	}
+
+	if (delNode->IsLeaf())
+	{
+		Node* parent = delNode->parent;
+		
+		if (parent == nullptr)
+			_root = nullptr;
+		else
+		{
+			if (parent->left == delNode)
+				parent->left = nullptr;
+			else
+				parent->right = nullptr;
+		}
+
+		delete delNode;
+		_size--;
+	}
+	else if (delNode->HasOnlyOneChild())
+	{
+		Node* parent = delNode->parent;
+		Node* child = delNode->left != nullptr ? delNode->left : delNode->right;
+
+		if (parent == nullptr)
+		{
+			_root = child;
+			child->parent == nullptr;
+		}
+		else
+		{
+			if (parent->left == delNode)
+				parent->left = child;
+			else
+				parent->right = child;
+
+			child->parent = parent;
+		}
+
+		delete delNode;
+		_size--;
+	}
+
+}
+
+template<typename TKey, typename TValue>
+inline void Tree<TKey, TValue>::Print() const 
+{
+	Print(_root);
+}
+
+template<typename TKey, typename TValue>
+inline void Tree<TKey, TValue>::Print(Node* node)const
+{
+	if (node == nullptr)
+		return;
+	
+	if (node->left != nullptr)
+		Print(node->left);	
+	
+	cout << node->key << " => " << node->value << endl;
+
+	if (node->right != nullptr)
+		Print(node->right);		
+	
+}
+
+template<typename TKey, typename TValue>
+inline TKey Tree<TKey, TValue>::MinKey() const
+{
+	return MinNode(_root)->key;
+}
+
+template<typename TKey, typename TValue>
+inline TValue& Tree<TKey, TValue>::operator[](TKey key)
+{
+	Node* node = SearchNode(key);
+	if (node == nullptr)
+	{
+		throw exception( "Error: key not found");		
+	}
+
+	return node->value;
+}
+
+template<typename TKey, typename TValue>
+inline const TValue& Tree<TKey, TValue>::operator[](TKey key) const
+{
+	Node* node = SearchNode(key);
+	if (node == nullptr)
+	{
+		cerr << "Error: key not found" << endl;
+	}
+
+	return node->value;
+}
